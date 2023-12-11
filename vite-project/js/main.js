dc20e25d22d11fa4f026655c812738832189acfb
jacksonz16 <jacksonz2390@gmail.com>
2023-12-11 08:59:47 -0500

    12345

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,10 +1,12 @@
-async function getData() {
+const URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=32d4d8eb5d3a4d59bbbef34cda51f943&page=1";
+console.log(fetch(URL))
 
-let res = await fetch(
-);
-"https://api.themoviedb.org/3/movie/top_rated? api_key=32d4d8eb5d3a4d59bbbef34cda51f943&page=1"
-let data = await res.json();
-console.log(data);
-data.results.forEach((movie) => console.log(movie));
+async function getData(URL){
+    try{
+        const response =await fetch (URL);
+        const data =await response.json();
+        console.log(data);
+    }catch(error){console.log(error)}
+
 }
-getData();
+getData(URL);
