0c20356fce2b2b5ddba7df7236f711bad0601bc7
jacksonz16 <jacksonz2390@gmail.com>
2023-12-12 09:27:26 -0500

    1

--- vite-project/js/api.js
+++ vite-project/js/api.js
@@ -1,10 +1,12 @@
-// const URL = "https://api.quotable.io/random"
-// async function getData(URL) {
-// try {
-// const response = await fetch (URL);
-// const data = await response.json(); //makes the data into JSON object we can use document.getElementById("api-response").textContent = data.content;
-// } catch (error) {
-// }
-// }
-// console.log(error);
-// getData(URL);
+const URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=32d4d8eb5d3a4d59bbbef34cda51f943&page=1";
+console.log(fetch(URL))
+
+async function getData(URL){
+    try{
+        const response =await fetch (URL);
+        const data =await response.json();
+        console.log(data);
+    }catch(error){console.log(error)}
+
+}
+getData(URL);
