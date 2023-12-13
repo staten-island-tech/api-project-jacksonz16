02363d1b011f28f046d0aa977c21df59acc1a6dd
jacksonz16 <jacksonz2390@gmail.com>
2023-12-13 09:05:32 -0500

    1

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,5 @@
 const URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=32d4d8eb5d3a4d59bbbef34cda51f943&page=1";
 console.log(fetch(URL))
-
 async function getData(URL){
     try{
         const response =await fetch (URL);
