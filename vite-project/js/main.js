85194b05e2d6e15e5b380d93e67dcc87509634a1
jacksonz16 <jacksonz2390@gmail.com>
2023-12-20 09:29:10 -0500

    11322131312

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,13 +1,51 @@
 import '../css/style.css'
 
-const URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=32d4d8eb5d3a4d59bbbef34cda51f943&page=2";
+const URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=32d4d8eb5d3a4d59bbbef34cda51f943&page=1";
 console.log(fetch(URL))
 
-async function getData(URL){
+
     try{
         const response =await fetch (URL);
         const data =await response.json();
         console.log(data);
     }catch(error){console.log(error)}
+
+getData(URL);
+
+async function getData(URL) {
+    try {
+        const response = await fetch(URL + "?" + new URLSearchParams({
+         
+
+        }));
+        if (response.status != 200) {
+            throw new Error(response.statusText);
+        }
+        const info = await response.json();
+        console.log(info);
+
+        data.movie.forEach((movie) => data.push(movie));
+
+
+    } catch (error) {
+        console.log(error);
+        document.querySelector("h1").textContent = "work technology work";
+    }
 }
 getData(URL);
+
+function insert(data) {
+    data.forEach((x) => {
+        DOMSelectors.container_2.insertAdjacentHTML(
+            "beforebegin",
+            `<div class= "card">
+    <h2 class= "title">${x.title}</h2>
+    <img scr="${x.image}" class="cardimg">
+    <h3 class=" movie" >${"movie" + x.movie}</h3>
+    <h3 class= "movielength " >${"Minutes" + x.movielength}</h3>
+    </div>`
+        )
+    })
+};
+
+
