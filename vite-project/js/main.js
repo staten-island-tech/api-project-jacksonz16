45f5dac62fd6cc76b2d2e27c30dd81531f10a8ec
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2023-12-19 23:17:00 -0500

    123

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,3 +1,3 @@
-fetch("https://genshinlist.com/api/characters")
+fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=32d4d8eb5d3a4d59bbbef34cda51f943&page=2")
     .then(response => response.json())
     .then(quote => console.log(quote))
