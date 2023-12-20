4a32c31a37258e87fab3aa2929dd959b100fba3b
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2023-12-19 22:46:28 -0500

    1

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,13 +1,3 @@
-import '../css/style.css'
-
-const URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=32d4d8eb5d3a4d59bbbef34cda51f943&page=2";
-console.log(fetch(URL))
-
-async function getData(URL){
-    try{
-        const response =await fetch (URL);
-        const data =await response.json();
-        console.log(data);
-    }catch(error){console.log(error)}
-}
-getData(URL);
+fetch('https://gsi.fly.dev/')
+    .then(response => response.json())
+    .then(quote => console.log(quote))
