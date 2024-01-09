468bcf84e0fe5f58e15a32bad90d4ab1d08b43eb
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2024-01-09 18:46:37 -0500

    1

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -26,7 +26,7 @@ function defaultCardGenerate(filtercharacters) {
           .toLowerCase()
           .replace(/ /g, "")}">
           <h2 class="characterName">${characters.name}</h2>
-          <img class="img" src="${characters.image}"/>
+          <img class="img" src="${characters.image}" alt="${characters.name}"class="img"/><img>
           <div class="text">
           <h3>${characters.status}</h3>
           <h3>${characters.origin.name}</h3>
@@ -42,3 +42,17 @@ function defaultCardGenerate(filtercharacters) {
     defaultCardGenerate(filtercharacters);
   }
   main();
+
+  async function search() {
+  const character = await document.getElementById("input").value.toLowerCase();
+  const charactercards = await document.getElementsByClassName("card");
+  for (var i = charactercards.length - 1; i >= 0; i--) {
+    if (!charactercards[i].id.includes(character)) {
+      charactercards[i].style.display = "none";
+    } else {
+      charactercards[i].style.display = "";
+    }
+  }
+}
+
+ 
