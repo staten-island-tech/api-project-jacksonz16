e7fa8f6dc516db6484c582e680e19bc170206359
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2024-01-09 19:28:18 -0500

    1

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,4 +1,6 @@
  export const DOMSelectors = {
     cards: document.querySelector(".cards"),
     search: document.getElementById("search"),
+     input: document.getElementById("input"),
+     card: document.getElementById("card"),
   };
