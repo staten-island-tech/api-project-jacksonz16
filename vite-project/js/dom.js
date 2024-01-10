d5d6f95648f8fc9f442dc1c5cae31cf45715559c
jacksonz16 <144455915+jacksonz16@users.noreply.github.com>
2024-01-10 18:27:45 -0500

    1

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,7 +1,9 @@
- export const DOMSelectors = {
+ export const DOMSelector = {
     cards: document.querySelector(".cards"),
     search: document.getElementById("search"),
      input: document.getElementById("input"),
      card: document.getElementById("card"),
-     error: document.getElementById("error")
+     searchForm: document.getElementById("search"),
+     searchBar: document.getElementById("search-bar"),
+     error: document.getElementById("error"),
   };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,11 +1,6 @@
 import '../css/style.css'
-import { DOMSelectors } from '../js/dom.js'
-
-
+import { DOMSelector } from '../js/dom.js'
 const url = "https://rickandmortyapi.com/api/character";
-
-
-
 
 async function getData(url) {
   try {
@@ -20,13 +15,13 @@ async function getData(url) {
 
 function defaultCardGenerate(filtercharacters) {
     filtercharacters.forEach((characters) => {
-      DOMSelectors.cards.insertAdjacentHTML(
+      DOMSelector.cards.insertAdjacentHTML(
         "beforeend",
         `<div class="card" id="${characters.name
           .toLowerCase()
           .replace(/ /g, "")}">
           <h2 class="characterName">${characters.name}</h2>
-          <img  src="${characters.image}" alt="${characters.name}"class="img"/><img>
+          <img src="${characters.image}" alt="${characters.name}"class="img"/>
           <div class="text">
           <h3>${characters.status}</h3>
           <h3>${characters.origin.name}</h3>
@@ -42,16 +37,9 @@ function defaultCardGenerate(filtercharacters) {
   }
   main();
 
-//   async function search() {
-//   const character = await document.getElementById("input").value.toLowerCase();
-//   const charactercards = await document.getElementsByClassName("card");
-//   for (var i = charactercards.length - 1; i >= 0; i--) {
-//     if (!charactercards[i].id.includes(character)) {
-//       charactercards[i].style.display = "none";
-//     } else {
-//       charactercards[i].style.display = "";
-//     }
-//   }
-// }
-
- 
+  DOMSelector.searchForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const  query = DOMSelector.searchBar.value
+    const URL = "https://rickandmortyapi.com/api/character/?name="+query
+    getData(URL);
+  });
