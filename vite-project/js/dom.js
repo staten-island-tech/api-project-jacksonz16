76800ff8af0f4ecfc543eaeea9ea7c8c6e55d226
jacksonz16 <jacksonz2390@gmail.com>
2024-01-12 08:54:52 -0500

    clean up dom slelectors

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,7 +1,5 @@
  export const DOMSelector = {
     cards: document.querySelector(".cards"),
-    search: document.getElementById("search"),
-     input: document.getElementById("input"),
      card: document.getElementById("card"),
      searchForm: document.getElementById("search"),
      searchBar: document.getElementById("search-bar"),
